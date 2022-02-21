a298adef924d1dc4912d712e76fa59e013c995cf
Ruben Cordero <ruben.cordero29@yahoo.com>
2022-02-20 23:46:11 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ruben
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isClicked: 'false',
+      inputValue: '',
+      listOfTodos: [],
+    };
+  }
+
+  handleClick = () => {
+    this.state.isClicked
+      ? this.setState({ isClicked: false })
+      : this.setState({ isClicked: true });
+  };
+
+  handleChange = (event) => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      listOfTodos: [...this.state.listOfTodos, this.state.inputValue],
+    });
+    this.setState({ inputValue: '' });
+  };
+
+  handleDelete = (index) => {
+    console.log('clicked')
+    let objectCopy = [...this.state.listOfTodos];
+    objectCopy.splice(index, 1);
+    this.setState({listOfTodos: [...objectCopy]})
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              value={this.state.inputValue}
+              onChange={this.handleChange}
+            ></input>
+            <button type="submit">Submit</button>
+          </form>
+          <ol>
+            {this.state.listOfTodos.map((todo, index) => {
+              return (
+                <li key={index}>
+                  {todo}
+                  <button onClick={() => this.handleDelete(index)}>
+                    Delete
+                  </button>
+                </li>
+              );
+            })}
+          </ol>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
